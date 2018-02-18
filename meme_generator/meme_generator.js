3cf077413ce13287a591ad500581f448a2ed4a84
teh-f00l <36428156+teh-f00l@users.noreply.github.com>
2018-02-18 08:28:10 -0500

    skeleton code

--- meme_generator/meme_generator.js
+++ meme_generator/meme_generator.js
@@ -1,17 +1,33 @@
+var input1,input2, image,img
+
+var input1,input2, image,img
+
 function setup() {
   createCanvas(500,500);
+  input1 = createInput();
+  input1.position(20, 65);
+  input2 = createInput();
+  input2.position(width/4, height*3/height/4);
+  img = createInput()
+  img.position(width/2,height/2)
+  image=createImg(img);
+  
 
 }
 
+
 function draw() {
-  text('word', 10, 30);
-  text('word', 60, 30);
+
+  text(input1.value(), 10, 30);
+  text(input2.value(), 200, 90);
   if (mouseIsPressed){
     fill(0);
     ellipse(mouseX,mouseY,10,10);
   } else {
     noFill(255);
+    
   }
+  
   
   
 }
